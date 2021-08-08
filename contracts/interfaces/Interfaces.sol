45b6a8d90ac03d3eeedc691de2023ecca54e6c7b
Michael De Luca <35537333+deluca-mike@users.noreply.github.com>
2021-08-08 14:52:53 -0400

    chore: Slight Cleanup (#3)
    * chore: Slight Cleanup
    
    * fix: reorder import alphabetically
    
    Co-authored-by: Lucas <lucas@maple.finance>

--- contracts/interfaces/Interfaces.sol
+++ contracts/interfaces/Interfaces.sol
@@ -2,9 +2,6 @@
 pragma solidity 0.6.11;
 
 interface IERC20DetailsLike {
-
     function symbol() external view returns (string memory);
-
     function decimals() external view returns (uint256);
-
 }
